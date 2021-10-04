c29a50eef55addb0d3079998f53da524d9488ed7
andreysobol <andreysobol@protonmail.com>
2021-10-04 15:59:21 +0300

    Create UnsafeMath.sol

---
+++ UnsafeMath.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+library UnsafeMath {
+
+    function unsafeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
+        unchecked {
+            return a + b;
+        }
+    }
+
+    function unsafeSub(uint256 a, uint256 b) internal pure returns (uint256) {
+        unchecked {
+            return a - b;
+        }
+    }
+
+    function unsafeMul(uint256 a, uint256 b) internal pure returns (uint256) {
+        unchecked {
+            return a * b;
+        }
+    }
+
+}
